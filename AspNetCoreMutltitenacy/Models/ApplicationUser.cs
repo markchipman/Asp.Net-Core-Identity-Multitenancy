161da7e2a962d7fee8ef2bb4405739261e635a14
Andreas Kuhlmann <Andreas.Kuhlmann@live.de>
2017-04-26 12:00:53 +0200

    initial commit

---
+++ AspNetCoreMutltitenacy/Models/ApplicationUser.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AspNetCore.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore.Models
+{
+    public interface ITenantIdProvider<out T>
+        where T : IEquatable<T>
+    {
+        T TenantId { get; }
+    }
+
+    public class ApplicationTenantIdProvider<TTenantId> : ITenantIdProvider<TTenantId>
+        where TTenantId : IEquatable<TTenantId>
+    {
+        public ApplicationTenantIdProvider(TTenantId teantId)
+        {
+            this.TenantId = teantId;
+        }
+        public TTenantId TenantId { get; }
+    }
+
+    public class ApplicationTenantIdProvider : ApplicationTenantIdProvider<string>
+    {
+        public ApplicationTenantIdProvider(string tenantId) : base(tenantId) { }
+    }
+
+    public class IdentiyUserMultiTenant<TKey, TTenantId> : IdentityUser<TKey>
+        where TKey : IEquatable<TKey>
+        where TTenantId : IEquatable<TTenantId>
+    {
+        public TTenantId TenantId { get; set; }
+    }
+
+    public class IdentiyRoleMultiTenant<TKey, TTenantId> : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+        where TTenantId : IEquatable<TTenantId>
+    {
+        public TTenantId TenantId { get; set; }
+    }
+
+    public class UserStoreMultiTenant<TUser, TRole, TKey, TTenantId> : UserStore<TUser, TRole, ApplicationDbContext, TKey>
+        where TUser : IdentiyUserMultiTenant<TKey, TTenantId>
+        where TRole : IdentiyRoleMultiTenant<TKey, TTenantId>
+        where TKey : IEquatable<TKey>
+        where TTenantId : IEquatable<TTenantId>
+    {
+        public TTenantId TenantKey { get; set; }
+        public UserStoreMultiTenant(ApplicationDbContext context, ITenantIdProvider<TTenantId> tenantProvider, IdentityErrorDescriber describer = null) : base(context, describer)
+        {
+            this.TenantKey = tenantProvider.TenantId;
+        }
+
+        public override Task<IdentityResult> CreateAsync(TUser user, CancellationToken cancellationToken = new CancellationToken())
+        {
+            user.TenantId = this.TenantKey;
+            return base.CreateAsync(user, cancellationToken);
+        }
+        public override Task<TUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            return Users.FirstOrDefaultAsync(u => u.NormalizedUserName == normalizedUserName && u.TenantId.Equals(this.TenantKey), cancellationToken);
+        }
+        public override Task<TUser> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            return Users.FirstOrDefaultAsync(u => u.NormalizedEmail == normalizedEmail && u.TenantId.Equals(this.TenantKey), cancellationToken);
+        }
+    }
+
+    public class RoleStoreMultiTenant<TRole, TKey, TTenantId> : RoleStore<TRole, ApplicationDbContext, TKey>
+        where TRole : IdentiyRoleMultiTenant<TKey, TTenantId>
+        where TKey : IEquatable<TKey>
+        where TTenantId : IEquatable<TTenantId>
+    {
+        public TTenantId TenantKey { get; set; }
+        public RoleStoreMultiTenant(ApplicationDbContext context, ITenantIdProvider<TTenantId> tenantProvider, IdentityErrorDescriber describer = null) : base(context, describer)
+        {
+            this.TenantKey = tenantProvider.TenantId;
+        }
+        public override async Task<IdentityResult> CreateAsync(TRole role, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            if (role == null)
+            {
+                throw new ArgumentNullException(nameof(role));
+            }
+            role.TenantId = this.TenantKey;
+            Context.Add(role);
+            if (AutoSaveChanges)
+            {
+                await Context.SaveChangesAsync(cancellationToken);
+            }
+            return IdentityResult.Success;
+        }
+        public override Task<TRole> FindByNameAsync(string normalizedName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ThrowIfDisposed();
+            return Roles.FirstOrDefaultAsync(r => r.NormalizedName == normalizedName && r.TenantId.Equals(this.TenantKey), cancellationToken);
+        }
+    }
+
+
+    public class ApplicationUser : IdentiyUserMultiTenant<Guid, string>
+    {
+    }
+
+    public class ApplicationRole : IdentiyRoleMultiTenant<Guid, string>
+    {
+    }
+
+
+    public class ApplicationUserStore : UserStoreMultiTenant<ApplicationUser, ApplicationRole, Guid, string>
+    {
+        public ApplicationUserStore(ApplicationDbContext context, ApplicationTenantIdProvider tenantProvider) : base(context, tenantProvider)
+        {
+            this.TenantKey = tenantProvider.TenantId;
+        }
+    }
+    public class ApplicationRoleStore : RoleStoreMultiTenant<ApplicationRole, Guid, string>
+    {
+        public ApplicationRoleStore(ApplicationDbContext context, ApplicationTenantIdProvider tenantProvider) : base(context, tenantProvider)
+        {
+            this.TenantKey = tenantProvider.TenantId;
+        }
+    }
+
+    // Add profile data for application users by adding properties to the ApplicationUser class
+}
---
+++ AspNetCoreMutltitenacy/Startup.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AspNet.Security.OpenIdConnect.Primitives;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using AspNetCore.Data;
+using AspNetCore.Models;
+using AspNetCore.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using OpenIddict.Core;
+using OpenIddict.Models;
+
+namespace AspNetCore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();  // <- ADD THIS
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //// Configure the context to use an in-memory store.
+                //options.UseInMemoryDatabase();
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+                // Register the entity sets needed by OpenIddict.
+                // Note: use the generic overload if you need
+                // to replace the default OpenIddict entities.
+                options.UseOpenIddict<Guid>();
+            });
+
+            services.AddScoped<IRoleStore<ApplicationRole>, ApplicationRoleStore>();
+            services.AddScoped<IUserStore<ApplicationUser>, ApplicationUserStore>();
+            services.AddScoped(serviceProvider =>new ApplicationTenantIdProvider("TenantId-1"));
+            services.AddIdentity<ApplicationUser, ApplicationRole>(o =>
+                {
+                    o.User.RequireUniqueEmail = false;
+                    o.Password.RequireDigit = false;
+                    o.Password.RequireLowercase = false;
+                    o.Password.RequireUppercase = false;
+                    o.Password.RequireNonAlphanumeric = false;
+                    o.Password.RequiredLength = 6;
+                }).AddUserStore<ApplicationUserStore>()
+                // Change last paramenter to the matching UserId type!
+                .AddEntityFrameworkStores<ApplicationDbContext, Guid>();
+
+            // Configure Identity to use the same JWT claims as OpenIddict instead
+            // of the legacy WS-Federation claims it uses by default (ClaimTypes),
+            // which saves you from doing the mapping in your authorization controller.
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+            });
+
+            // Register the OpenIddict services.
+            services.AddOpenIddict<Guid>(options =>
+            {
+                // Register the Entity Framework stores.
+                options.AddEntityFrameworkCoreStores<ApplicationDbContext>();
+
+                // Register the ASP.NET Core MVC binder used by OpenIddict.
+                // Note: if you don't call this method, you won't be able to
+                // bind OpenIdConnectRequest or OpenIdConnectResponse parameters.
+                options.AddMvcBinders();
+
+                // Enable the token endpoint.
+                options.EnableTokenEndpoint("/connect/token");
+
+                // Enable the password flow.
+                options.AllowPasswordFlow().AllowRefreshTokenFlow(); 
+
+                // During development, you can disable the HTTPS requirement.
+                options.DisableHttpsRequirement();
+
+                // Note: to use JWT access tokens instead of the default
+                // encrypted format, the following lines are required:
+                //
+                // options.UseJsonWebTokens();
+                // options.AddEphemeralSigningKey();
+            });
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseCors(builder =>
+            {
+                //builder.WithOrigins("http://localhost:5055");
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowAnyOrigin();
+            });
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+            // Add a middleware used to validate access
+            // tokens and protect the API endpoints.
+            app.UseOAuthValidation();
+
+            // If you prefer using JWT, don't forget to disable the automatic
+            // JWT -> WS-Federation claims mapping used by the JWT middleware:
+            //
+            // JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            // JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            //
+            // app.UseJwtBearerAuthentication(new JwtBearerOptions
+            // {
+            //     Authority = "http://localhost:58795/",
+            //     Audience = "resource_server",
+            //     RequireHttpsMetadata = false,
+            //     TokenValidationParameters = new TokenValidationParameters
+            //     {
+            //         NameClaimType = OpenIdConnectConstants.Claims.Subject,
+            //         RoleClaimType = OpenIdConnectConstants.Claims.Role
+            //     }
+            // });
+
+            // Alternatively, you can also use the introspection middleware.
+            // Using it is recommended if your resource server is in a
+            // different application/separated from the authorization server.
+            //
+            // app.UseOAuthIntrospection(options =>
+            // {
+            //     options.Authority = new Uri("http://localhost:58795/");
+            //     options.Audiences.Add("resource_server");
+            //     options.ClientId = "resource_server";
+            //     options.ClientSecret = "875sqd4s5d748z78z7ds1ff8zz8814ff88ed8ea4z4zzd";
+            //     options.RequireHttpsMetadata = false;
+            // });
+
+            app.UseOpenIddict();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            //app.UseMvcWithDefaultRoute();
+            app.UseWelcomePage();
+            // Make sure to drop unique indexes on rolenames username and emails, which are created by default from Asp.Net Core Identity!!
+            InitializeAsync(app.ApplicationServices, CancellationToken.None).GetAwaiter().GetResult();
+        }
+        private async Task InitializeAsync(IServiceProvider services, CancellationToken cancellationToken)
+        {
+            // Create a new service scope to ensure the database context is correctly disposed when this methods returns.
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                await context.Database.EnsureCreatedAsync(cancellationToken);
+                try
+                {
+                    await context.Database.ExecuteSqlCommandAsync("drop index UserNameIndex on AspNetUsers", cancellationToken);
+                }
+                catch
+                {
+                    // ignored
+                }
+                try
+                {
+                    await context.Database.ExecuteSqlCommandAsync("drop index EmailIndex on AspNetUsers", cancellationToken);
+                }
+                catch
+                {
+                    // ignored
+                }
+                try
+                {
+                    await context.Database.ExecuteSqlCommandAsync("drop index RoleNameIndex on AspNetRoles", cancellationToken);
+                }
+                catch
+                {
+                    // ignored
+                }
+                // Seed the database with the sample data....
+            }
+        }
+    }
+}
